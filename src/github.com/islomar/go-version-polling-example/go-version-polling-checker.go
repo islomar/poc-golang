bb56da9fe22d0191011d52e173acdeb546429be7
Isidro Lopez <islomar@gmail.com>
2015-11-16 14:15:17 +0100

    Add monitoring to Go version polling application

--- src/github.com/islomar/go-version-polling-example/go-version-polling-checker.go
+++ src/github.com/islomar/go-version-polling-example/go-version-polling-checker.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"log"
 	"html/template"
+	"expvar"
 )
 
 const changeURL = "https://code.google.com/p/go/source/detail?r=go1.5"
@@ -14,6 +15,11 @@ const changeURL = "https://code.google.com/p/go/source/detail?r=go1.5"
 var (
 	httpAdr = flag.String("http", "localhost:8080", "Liste address")
 	pollPeriod   = flag.Duration("poll", 5*time.Second, "Poll period")
+
+	hitCount		= expvar.NewInt("hitCount")
+	pollCount		= expvar.NewInt("pollCount")
+	pollError		= expvar.NewString("pollError")
+	pollErrorCount	= expvar.NewInt("pollErrorCount")
 )
 
 var state struct {
@@ -51,15 +57,19 @@ func poll(period time.Duration) {
 }
 
 func isTagged() bool {
+	pollCount.Add(1)
 	r, err := http.Head(changeURL)
 	if err != nil {
 		log.Print(err)
+		pollError.Set(err.Error())
+		pollErrorCount.Add(1)
 		return false
 	}
 	return r.StatusCode == http.StatusOK
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	hitCount.Add(1)
 	state.RLock()
 	data := struct {
 		Yes bool
